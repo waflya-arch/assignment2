3cb246ae315fbf077908506127e7817891ba4eba
waflya <bakikairstudy@gmail.com>
2025-12-30 11:28:04 +0500

    feat: create Driver class with name and license attributes

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    private String name;
+    private String licenseNumber;
+
+    public Driver(String name, String licenseNumber) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public void displayDriverInfo() {
+        System.out.println("Driver: " + name + " License: " + licenseNumber);
+    }
+}
