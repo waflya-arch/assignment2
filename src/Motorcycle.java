914fdf663024ce089c7fb2c5a3c6158245182b59
waflya <bakikairstudy@gmail.com>
2025-12-30 11:27:23 +0500

    feat: implement Motorcycle subclass with sidecar attribute

---
+++ src/Motorcycle.java
@@ -0,0 +1,18 @@
+public class Motorcycle extends Vehicle {
+    private boolean hasSidecar;
+
+    public Motorcycle(String brand, int year, boolean hasSidecar) {
+        super(brand, year);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println(brand + " engine starts");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println(brand + " engine stopped");
+    }
+}
